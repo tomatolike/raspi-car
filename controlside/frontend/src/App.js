0a917015a54faf0b577a96f1fe6dfef8cdb93dd6
tomatolike <695268122@qq.com>
2020-09-20 03:44:30 -0400

    web control v1 tested on phone

--- controlside/frontend/src/App.js
+++ controlside/frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import apicallpost from './component/POST'
 import apicallget from './component/GET'
-import {Container, Row, Col} from 'react-bootstrap'
+import {Container, Row, Col, Table, Button} from 'react-bootstrap'
 
 class App extends Component {
   constructor(props) {
@@ -25,6 +25,8 @@ class App extends Component {
     this.updateStatus = this.updateStatus.bind(this);
     this.apiget = this.apiget.bind(this);
     this.apicall = this.apicall.bind(this);
+    this.touchstart = this.touchstart.bind(this);
+    this.touchend = this.touchend.bind(this);
   }
 
   keydown(event){
@@ -43,6 +45,22 @@ class App extends Component {
     }
   }
 
+  touchstart(event){
+    // console.log(event.target.id)
+    if(event.target.id == 'w'){
+      this.setState({W:true,last:'W'})
+    }
+    if(event.target.id == 's'){
+      this.setState({W:true,last:'S'})
+    }
+    if(event.target.id == 'a'){
+      this.setState({W:true,last:'A'})
+    }
+    if(event.target.id == 'd'){
+      this.setState({W:true,last:'D'})
+    }
+  }
+
   keyup(event){
     // console.log(event.keyCode);
     if(event.keyCode==87){
@@ -56,6 +74,22 @@ class App extends Component {
     }
     if(event.keyCode==68){
       this.setState({D:false})
+    }
+  }
+
+  touchend(event){
+    // console.log(event)
+    if(event.target.id == 'w'){
+      this.setState({W:false,last:'W'})
+    }
+    if(event.target.id == 's'){
+      this.setState({W:false,last:'S'})
+    }
+    if(event.target.id == 'a'){
+      this.setState({W:false,last:'A'})
+    }
+    if(event.target.id == 'd'){
+      this.setState({W:false,last:'D'})
     }
   }
 
@@ -131,6 +165,29 @@ class App extends Component {
             moving:{this.state.status}<br/>
             network:{this.state.connected}<br/>
             address:{this.state.addr}<br/>
+          </Col>
+        </Row>
+        <Row style={{minHeight:"25vh", display:"flex", flexWrap:"wrap",alignItems:'center',alignContent:'center'}}>
+          <Col style={{flexGrow:1}}>
+            <Table striped bordered hover>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td><Button id='w' onMouseDown={this.touchstart} onTouchStart={this.touchstart} onTouchEnd={this.touchend} onMouseUp={this.touchend} >W</Button></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td><Button id='a' onMouseDown={this.touchstart} onTouchStart={this.touchstart} onTouchEnd={this.touchend} onMouseUp={this.touchend} >A</Button></td>
+                  <td></td>
+                  <td><Button id='d' onMouseDown={this.touchstart} onTouchStart={this.touchstart} onTouchEnd={this.touchend} onMouseUp={this.touchend} >D</Button></td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td><Button id='s' onMouseDown={this.touchstart} onTouchStart={this.touchstart} onTouchEnd={this.touchend} onMouseUp={this.touchend} >S</Button></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </Table>
           </Col>
         </Row>
         <Row style={{minHeight:"25vh", display:"flex", flexWrap:"wrap",alignItems:'center',alignContent:'center'}}>
